3cd6391d2128d0d547c1f8451f6928a17f188247
Kabelo Moiloa <moiloakabelo@gmail.com>
2016-07-27 18:47:03 +0200

    Add basic template

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+// Hello World (a program so useful that I've written it maybe 10 times)
+// Name: Kabelo Moiloa
+// Student Number: MLXKAB004
+// Date: 27 July 2016
+
+class HelloWorld
+{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
